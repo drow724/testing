15d32815018218c3014d7ce7d47704861a06d47b
Song4025 <thdgus4025@gmail.com>
2023-08-15 16:57:15 +0900

    RepeatedTest2

--- src/test/java/com/example/demo/StudyTest.java
+++ src/test/java/com/example/demo/StudyTest.java
@@ -19,7 +19,19 @@ import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -31,11 +43,65 @@ public class StudyTest {
 		System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
 	}
 	
-	@DisplayName("스터디 만들기")
+	@DisplayName("스터디 만들기 with value source")
 	@ParameterizedTest(name = "{index} {displayName}, message={0}")
 	@ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
+	@EmptySource
+	@NullSource
+	@NullAndEmptySource
 	void parameterizedTest(String message) {
 		System.out.println(message);
+	}
+	
+	@DisplayName("스터디 만들기 with converter")
+	@ParameterizedTest(name = "{index} {displayName}, message={0}")
+	@ValueSource(ints = {10, 20, 40})
+	@CsvSource({"10, '자바 스터디'", "20, '스프링 스터디'"})
+	void parameterizedTestWithConverter(@ConvertWith(StudyConverter.class) Study study) {
+		System.out.println(study.getLimit());
+	}
+	
+	static class StudyConverter extends SimpleArgumentConverter {
+
+		@Override
+		protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+			assertEquals(Study.class, targetType, "Can only convert to Study");
+			return new Study(Integer.parseInt(source.toString()));
+		}
+		
+	}
+	
+	@DisplayName("스터디 만들기 with argumentAccessor")
+	@ParameterizedTest(name = "{index} {displayName}, message={0}")
+	@CsvSource({"10, '자바 스터디'", "20, '스프링 스터디'"})
+	void parameterizedTestWithArgumentsAccessor(ArgumentsAccessor argumentsAccessor) {
+		Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+		System.out.println(study);
+	}
+	
+	@DisplayName("스터디 만들기 with argumentsAggregator")
+	@ParameterizedTest(name = "{index} {displayName}, message={0}")
+	@CsvSource({"10, '자바 스터디'", "20, '스프링 스터디'"})
+	void parameterizedTestWithArgumentsAggregator(@AggregateWith(StudyAggregator.class) Study study) {
+		System.out.println(study);
+	}
+	
+	static class StudyAggregator implements ArgumentsAggregator {
+
+		@Override
+		public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context)
+				throws ArgumentsAggregationException {
+			return new Study(accessor.getInteger(0), accessor.getString(1));
+		}
+		
+	}
+	
+	@DisplayName("스터디 만들기 with cvs")
+	@ParameterizedTest(name = "{index} {displayName}, message={0}")
+	@CsvSource({"10, '자바 스터디'", "20, '스프링 스터디'"})
+	void parameterizedTestWithCvsAggregate(Integer limit, String name) {
+		Study study = new Study(limit, name);
+		System.out.println(study);
 	}
 	
 	@FastTest
